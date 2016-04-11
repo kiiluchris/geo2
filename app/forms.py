9a34a0b3d12e457797f47e3dce506a632ff01095
CKiilu <cndeti@gmail.com>
2016-04-11 11:47:48 +0300

    Fixed form widget

--- app/forms.py
+++ app/forms.py
@@ -4,17 +4,13 @@ from django import forms
 from .models import SimplePlace, Skills
 
 class PlaceForm(forms.ModelForm):
-	queryset = Skills.objects.values('skill')
-	CHOICES = []
-	count = 0
-	for choice in queryset:		
-		CHOICES.append((count, choice['skill']))
-		count += 1
-	print CHOICES
-	skills = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, required= True, choices = CHOICES)
 	class Meta:
 		model = SimplePlace
 		fields = [
 		'city',
 		'location',
+		'skills'
 		]
+		widgets = {
+			'skills': forms.CheckboxSelectMultiple
+		}
