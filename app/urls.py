d713dd5ecdd3e2d5e6056d001e0864a8c6954545
CKiilu <cndeti@gmail.com>
2016-04-12 12:11:31 +0300

    Removed calendar

--- app/urls.py
+++ app/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     
-    url(r'^$', views.home, name="home"),
+    # url(r'^$', views.home, name="home"),
     url(r'^page(?P<id>[0-9]+)/$', views.page, name="page"),
     url(r'^event/type/([^/]+)/$', views.event_type, name='events'),
-    url(r'^event/type/', views.event_home, name='eventhome'),
+    url(r'^$', views.event_home, name='eventhome'),
     url(r'^events/', include('swingtime.urls')),
 ]
